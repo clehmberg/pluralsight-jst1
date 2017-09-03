2b275a6e478575e7caedc908cafd9111327d1b17
clehmberg <clintlehmberg@gmail.com>
2017-09-03 09:28:17 -0600

    learning variab, introducon to control flow

--- pluralsight-jst1/main.js
+++ pluralsight-jst1/main.js
@@ -1,15 +1,6 @@
-console.log(300 / 2);
-console.log(typeof(300 / 2));
-//number
-console.log("hello" + "world");
-console.log(typeof("hello" + "world"));
-//string
-console.log(4 < 2);
-console.log(typeof(4 < 2));
-//console.log(a + b);
-//console.log(typeof(a + b));
-
-console.log(3 + "6");
-console.log(typeof(3 + "6"));
-//string
-console.log(var a = 2);
+var answer = prompt("What is the capital of New York?");
+if(answer === "Albany"){
+  alert("You are correct!");
+}else {
+  alert("Sorry, " + answer + " is not correct.");
+}
