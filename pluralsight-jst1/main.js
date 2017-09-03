ff522df42c6882055888995038319481688d73a0
clehmberg <clintlehmberg@gmail.com>
2017-09-02 23:15:27 -0600

    concatonation,boolean

--- pluralsight-jst1/main.js
+++ pluralsight-jst1/main.js
@@ -1,10 +1,7 @@
-
-
-
-
-
-
-
-console.log("hello world");
-console.warn("Warning");
-console.error("error");
+console.log("a string is any value in quotations");
+console.log(2 + " for funnsies");
+console.log("hello" + " world");
+console.log("hello" + " " + "world");
+console.log(10 > 2);
+console.log(10 < 2);
+console.log(typeof(10 < 2));
