066e90788db0f612cb180844679aee6a7b788b17
clehmberg <clintlehmberg@gmail.com>
2017-09-02 23:34:45 -0600

    learn typeof

--- pluralsight-jst1/main.js
+++ pluralsight-jst1/main.js
@@ -1,7 +1,15 @@
-console.log("a string is any value in quotations");
-console.log(2 + " for funnsies");
-console.log("hello" + " world");
-console.log("hello" + " " + "world");
-console.log(10 > 2);
-console.log(10 < 2);
-console.log(typeof(10 < 2));
+console.log(300 / 2);
+console.log(typeof(300 / 2));
+//number
+console.log("hello" + "world");
+console.log(typeof("hello" + "world"));
+//string
+console.log(4 < 2);
+console.log(typeof(4 < 2));
+//console.log(a + b);
+//console.log(typeof(a + b));
+
+console.log(3 + "6");
+console.log(typeof(3 + "6"));
+//string
+console.log(var a = 2);
