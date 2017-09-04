dbb1d4047cc629da5e6b1087da39120ab4f285be
clehmberg <clintlehmberg@gmail.com>
2017-09-04 16:18:26 -0600

    pluralsight js assignment first draft

--- pluralsight-jst1/main.js
+++ pluralsight-jst1/main.js
@@ -1,12 +1,13 @@
-alert("It is the zombie apocalypse. You are looting a store and suddenly a zombie bursts through the door!");
-var weapon = prompt(":You search around frantically for a weapon. What do you choose? A bow and arrow, an axe or a rubber chicken");
-var randomNumber = Math.round(Math.random());
+alert("You have just been marooned on a deserted island. " + "You only have a pocket knife and a fire starter. " + "What do you do first?");
+var action = prompt("Take off your clothes and run around naked. " + "Start a fire. " + "Build a shelter.");
+//var randomNumber = Math.round(Math.random());
 
-alert("You attack the zombie with your " + weapon);
+//alert(action);
 
-if (randomNumber === 0 ){
-  alert("The zombie bites you. You lose!!!");
-}else if (randomNumber === 1 ) {
-  alert("You  kill the zombie with your " + weapon + " You win!!!");
-
+if (action === "start a fire" ){
+  alert("You built a fire to stay warm.");
+}else if (action === "build a shelter" ) {
+  alert("You have built a shelter to stay dry.");
+}else if (action === "take off your clothes and run around naked"){
+  alert("You got hypothermia and died " + "You lose!!!")
 }
