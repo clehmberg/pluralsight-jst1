a5be1de26ec90cd7fcbf2f3b7b62097bedfb9cf1
clehmberg <clintlehmberg@gmail.com>
2017-09-04 15:32:57 -0600

    Fizz Buzz continued, control flow and comparison operor

--- pluralsight-jst1/main.js
+++ pluralsight-jst1/main.js
@@ -1,12 +1,26 @@
-var randomNumber = Math.round( Math.random() * 5 );
+//var randomNumber = Math.round( Math.random() * 5 );
+var randomNumber = 15;
 
 
-
-if(randomNumber%3 === 0){
+if(randomNumber%3 === 0 && randomNumber != 0){
   alert("Fizz");
-}else if(randomNumber%5 === 0){
+}else if(randomNumber%5 === 0 && randomNumber != 0){
   alert("Buzz");
 }
+else if(randomNumber%3 === 0 && randomNumber%5 === 0){
+  alert ("Fizz Buzz");
+}
+
+if(randomNumber%3 === 0 && randomNumber != 0){
+  if(randomNumber%5 === 0){
+    alert("Fizz Buzz");
+
+  }
+  else{
+    alert(Fizz);
+  }
+}
+else if  (randomNumber%5 === 0 && randomNumber != 0);
 else {
   console.log(randomNumber);
 }
