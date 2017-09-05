cb6f22f0a60aac63d8f28f23dc2fab1983245eda
clehmberg <clintlehmberg@gmail.com>
2017-09-04 21:33:29 -0600

    island text , cant figure out the operator issue

--- pluralsight-jst1/main.js
+++ pluralsight-jst1/main.js
@@ -1,13 +1,63 @@
-alert("You have just been marooned on a deserted island. " + "You only have a pocket knife and a fire starter. " + "What do you do first?");
+var randomNumber = Math.round(Math.random());
+console.log(randomNumber);
+alert("You have just been marooned on an island. " + "You only have a pocket knife and a fire starter. " + "What do you do first?");
 var action = prompt("Take off your clothes and run around naked. " + "Start a fire. " + "Build a shelter.");
-//var randomNumber = Math.round(Math.random());
+//var randomNumber = 0;
 
-//alert(action);
+  if (action === "start a fire");{
+    alert("You built a fire.");
+  }var randomNumber = Math.round(Math.random());
+    console.log(randomNumber);
+if (randomNumber%2 === 0){
+  alert("Your fire went out. " + "You got hypothermia and died. ");{
+    alert ("You lose!!!");
+throw error();
+}
+  }else if (randomNumber%3 === 1)
+  alert("It's morning your fire kept you warm all night");{
+  var action2 = prompt("what do you do next? " + "Search the island. " + "Search for food ");
+}if (action2 = "search the island");{
+  alert("Your searching the island.");
+} var randomNumber = Math.round(Math.random());
+console.log(randomNumber);
+if (randomNumber%3 === 1){
+    alert("You found a five star resort and live happily ever after. " + "Yay!!!");
+    alert("You win!!!");
+    throw error ();
+  }else if (randomNumber%2 === 0){
+    alert("You stumble upon a tribe of cannibals and they eat you for lunch.");{
+    alert ("You lose!!!");
+throw error();
+  }
+}if (action === "build a shelter"){
+    alert("You have built a shelter.");
+} var randomNumber = Math.round(Math.random());{
+  console.log(randomNumber);
+}if (randomNumber%3 === 1){
+    alert("Your shelter blew away in the night. " + "You got hypothermia and died. ");{
+      alert ("You lose!!!");
+  throw error();
 
-if (action === "start a fire" ){
-  alert("You built a fire to stay warm.");
-}else if (action === "build a shelter" ) {
-  alert("You have built a shelter to stay dry.");
-}else if (action === "take off your clothes and run around naked"){
-  alert("You got hypothermia and died " + "You lose!!!")
+}
+}else if (randomNumber%2 === 0){
+    alert("It's morning, your shelter kept you dry all night");
+var action2 = prompt("what do you do next? " + "Search the island. " + "Search for food ");{
+if (action2 = "search the island");
+  alert("your searching the island");
+  var randomNumber = Math.round(Math.random());{
+  console.log(randomNumber);
+  }
+  }if (randomNumber%2 === 0){
+      alert("You found a five star resort and live happily ever after. " + "Yay!!!");
+      alert("You win!!!");
+      throw error ();
+  }  else if (randomNumber%3 === 1){
+      alert("You stumble upon a tribe of cannibals and they eat you for lunch.");
+      alert ("You lose!!!");
+  throw error();
+}
+}if (action === "take off your clothes and run around naked"){
+  alert("You got hypothermia and died " );
+  alert ("You lose!!!");
+throw error();
 }
