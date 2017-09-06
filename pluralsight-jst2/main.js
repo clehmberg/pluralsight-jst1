f31479d279634f5b5e413099e2c90de98deadd90
clehmberg <clintlehmberg@gmail.com>
2017-09-05 22:07:30 -0600

    aother while loop excersize

--- pluralsight-jst2/main.js
+++ pluralsight-jst2/main.js
@@ -1,11 +1,17 @@
-var i = 0;
-/*do {
-  console.log(i);
-  i++;
-}while (i < 11);
-*/
-while (i < 11){
-  console.log(i);
-  i++;
+var sad = confirm ("would you like to be cheered up?");
 
+while (sad) {
+  alert("what do you call a boomerang that doesn;t come back?...A stick!");
+  var yes = confirm ("Would you like to continue our session?");
+
+  if (yes) {
+    var response = prompt ("Tell me about your troubles:", "Type your feelings here");
+
+    if (response){
+      alert ("I am sorry you are feeling down about that. I know you can't see it, but I, the computer, will now give you a hug.  **hug**");
+      sad = false;
+    }
+  }else {
+    sad = false;
+  }
 }
