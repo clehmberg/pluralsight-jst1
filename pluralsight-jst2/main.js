a49e7ad91c982c581237036cc632faf9d5f1643b
clehmberg <clintlehmberg@gmail.com>
2017-09-05 21:09:15 -0600

    for loop basics

--- pluralsight-jst2/main.js
+++ pluralsight-jst2/main.js
@@ -1,16 +1,3 @@
-var sign = prompt("what is your astrological sign?").toLowerCase();
-alert ("Sensing...sensing your future.");
-switch(sign) {
-  case "taurus":
-  alert ("Your day will be awesome!");
-  break;
-  case "virgo":
-  alert ("Your day will be rainy");
-  break;
-  case "leo":
-  alert ("Your day will suck");
-  break;
-  default:
-  alert ("please enter a valid sign");
-  break;
+for (var i = 0; i < 11; i++){
+  console.log(i);
 }
