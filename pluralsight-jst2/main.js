831728055c2cc140b92395a6a7c53f1c25a20dad
clehmberg <clintlehmberg@gmail.com>
2017-09-05 21:55:00 -0600

    do while and while loops introduction

--- pluralsight-jst2/main.js
+++ pluralsight-jst2/main.js
@@ -1,14 +1,11 @@
-for (var i = 1; i <= 100; i++){
-if (i%3 === 0){
-  if (i%5 === 0){
-    console.log("fizzbuzz");
-  }
-  console.log("fizz");
-}
-else if(i%5 === 0){
-  console.log("buzz");
-}
-else {
+var i = 0;
+/*do {
   console.log(i);
-}
+  i++;
+}while (i < 11);
+*/
+while (i < 11){
+  console.log(i);
+  i++;
+
 }
