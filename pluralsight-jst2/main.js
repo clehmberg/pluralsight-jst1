6ee89f5c39fab1397921316076cbb4ecce957800
clehmberg <clintlehmberg@gmail.com>
2017-09-10 08:35:37 -0600

    introduction to functions

--- pluralsight-jst2/main.js
+++ pluralsight-jst2/main.js
@@ -1,17 +1,7 @@
-var sad = confirm ("would you like to be cheered up?");
-
-while (sad) {
-  alert("what do you call a boomerang that doesn;t come back?...A stick!");
-  var yes = confirm ("Would you like to continue our session?");
-
-  if (yes) {
-    var response = prompt ("Tell me about your troubles:", "Type your feelings here");
-
-    if (response){
-      alert ("I am sorry you are feeling down about that. I know you can't see it, but I, the computer, will now give you a hug.  **hug**");
-      sad = false;
-    }
-  }else {
-    sad = false;
-  }
+function sayHello(name){
+console.log("Oh hi " + name);
 }
+sayHello("Mark");
+sayHello("LIsa");
+sayHello("Denny");
+sayHello();
