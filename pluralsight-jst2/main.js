b5ca7dac5b2d35cf7df38d098cd8bd212ce1fae2
clehmberg <clintlehmberg@gmail.com>
2017-09-10 08:59:51 -0600

    indroduction to array's

--- pluralsight-jst2/main.js
+++ pluralsight-jst2/main.js
@@ -1,7 +1,3 @@
-function sayHello(name){
-console.log("Oh hi " + name);
-}
-sayHello("Mark");
-sayHello("LIsa");
-sayHello("Denny");
-sayHello();
+var friends = ["Mark","Lisa","Denny"];
+friends[3] = "Bob";
+friends.push("Mary");
