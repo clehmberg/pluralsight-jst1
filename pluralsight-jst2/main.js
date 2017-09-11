045cb5160c6294b8e6083edcabe132ad5d6a0714
clehmberg <clintlehmberg@gmail.com>
2017-09-11 15:12:55 -0600

    finishedplurasight lesson 1 assignment

--- pluralsight-jst2/main.js
+++ pluralsight-jst2/main.js
@@ -1,76 +1,67 @@
 
-//alert ("what to do first");
-function randomNumber(){
-return Math.round(Math.random() * 2);
+alert ("You are stranded on a deserted island with only a fire starter and a pocket knife. What do you do first?");
+var fire = "start a fire";
+var shelter = "build a shelter";
+var clothes = "take off your clothes and run around naked";
+var search = "Search the island";
+var food = "forage for food"
 
+var action1 = prompt ("Take off your clothes and run around naked(clothes), start a fire(fire) or build a shelter(shelter)");{
+  if (action1 === "fire"){
+   alert("You started a fire.");
 }
-//var randomNumber = (0);
-  console.log(randomNumber() );
 
-/*var action1 = prompt ("1 or 2");{
-  if (action1 === "2"){
-      alert("You built a fire.");
-}
-else {
+else if(action1 === "shelter") {
         alert("you built a shelter.");
 }
+else if(action1 === "clothes"){
+  alert("You got hypothermia and died....You lose");
 }
-*/
-if (randomNumber() <= 1){
-    alert("go to next")
-}
-  else{
-    alert("you lose")
-  }
-
-/*if(alert = "go to next"){
-  var action3 = prompt("choose 1, 2, or 3.")
-}
-if (action3 === "1"){
-  alert("you chose 1");
-}
-else if(action3 === "2"){
-alert("you chose 2")
-}
-else if(action3 === "3"){
-  alert("you chose 3")
-}
-/*randomNumber();{
- console.log(randomNumber() + "c");
-
-  if(randomNumber === 1){
-    var action2 = prompt("what next? 1 or 2?")
-  }
-  if (action2 === "1"){
-    alert("go to next2")
-  }
-  else{
-    alert("go to next3")
-  }
 }
 
-
-
-
-
-
-
-
-
-/*function randomNumber(range){
-return  Math.round(Math.random() + range );
+if(action1 === "fire"){
+var randomNumber = Math.round(Math.random() * 5);
 console.log(randomNumber);
+if (randomNumber <= 3){
+    alert("It's morning, your fire kept you warm all night. What do you do next?");
 }
-
-*/
-//alert(action1 [ randomNumber(action1.length - 2) ] );
-//var action1 = randomNumber(2);
-
-
-/*var hour = (randomNumber);
-if (hour < 2){
-  alert ("good day");
-}else{
-  alert("good evening")
+  else if(randomNumber >= 4){
+    alert("You didn't put enough wood on the fire and it went out in the night...");
+    alert("you got hypothermia and died...you lose")
+  }
 }
-*/
+  if(action1 === "shelter"){
+  var randomNumber = Math.round(Math.random() * 5);
+  console.log(randomNumber);
+if (randomNumber <= 3){
+    alert("It's morning, your shelter kept you dry all night. What do you do next?");
+}
+  else if(randomNumber >= 4){
+    alert("Your shelter blew away in the night...You got hypothermia and died...you lose");
+  }
+}
+if(randomNumber <= 3){
+  var action3 = prompt("search the island(search) or forage for food(food)");
+}
+var randomNumber = Math.round(Math.random() * 5);
+console.log(randomNumber);
+if (action3 === "search"){
+  alert("Your are searching the island!");
+  if(randomNumber <= 3){
+    alert("You found a five star resort and live happily ever after!...You win!")
+  }
+  else if(randomNumber >= 4){
+    alert("You stumble upon a tribe of cannibals during a religious ceremony and they eat you for lunch... You lose!")
+  }
+}
+else if(action3 === "food"){
+alert("You begin looking for food.")
+
+var randomNumber = Math.round(Math.random() * 5);
+console.log(randomNumber);
+if(randomNumber <= 3){
+  alert("You found a five star resort and live happily ever after!... You win!");
+}  else if(randomNumber >= 4){
+  alert("While searching the bushes for berries you got bitten by a poisonous snake and died...you lose");
+}
+}
