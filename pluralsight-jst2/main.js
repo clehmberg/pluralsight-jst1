f7a21bf1cb07c1f3aa19cb889e634a8385da91d4
clehmberg <clintlehmberg@gmail.com>
2017-09-11 12:02:49 -0600

    retry lesson 2 assignment, can't get randoNumberif else statements to work

--- pluralsight-jst2/main.js
+++ pluralsight-jst2/main.js
@@ -1,10 +1,76 @@
-var friends = ["Mark","Lisa","Denny","Peter","Steven","Claudet"];
 
-function greetfriends(){
-  for(var i = 0; i < friends.length; i++){
-    console.log("Oh hi " + friends[i]);
+//alert ("what to do first");
+function randomNumber(){
+return Math.round(Math.random() * 2);
+
+}
+//var randomNumber = (0);
+  console.log(randomNumber() );
+
+/*var action1 = prompt ("1 or 2");{
+  if (action1 === "2"){
+      alert("You built a fire.");
+}
+else {
+        alert("you built a shelter.");
+}
+}
+*/
+if (randomNumber() <= 1){
+    alert("go to next")
+}
+  else{
+    alert("you lose")
+  }
+
+/*if(alert = "go to next"){
+  var action3 = prompt("choose 1, 2, or 3.")
+}
+if (action3 === "1"){
+  alert("you chose 1");
+}
+else if(action3 === "2"){
+alert("you chose 2")
+}
+else if(action3 === "3"){
+  alert("you chose 3")
+}
+/*randomNumber();{
+ console.log(randomNumber() + "c");
+
+  if(randomNumber === 1){
+    var action2 = prompt("what next? 1 or 2?")
+  }
+  if (action2 === "1"){
+    alert("go to next2")
+  }
+  else{
+    alert("go to next3")
   }
 }
-greetfriends();
 
-friends[3];
+
+
+
+
+
+
+
+
+/*function randomNumber(range){
+return  Math.round(Math.random() + range );
+console.log(randomNumber);
+}
+
+*/
+//alert(action1 [ randomNumber(action1.length - 2) ] );
+//var action1 = randomNumber(2);
+
+
+/*var hour = (randomNumber);
+if (hour < 2){
+  alert ("good day");
+}else{
+  alert("good evening")
+}
+*/
