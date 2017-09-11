340ddfc80b56ed670f314ec4a475508f253d6c4b
clehmberg <clintlehmberg@gmail.com>
2017-09-11 12:22:21 -0600

    introduction to objects

---
+++ pluralsight-jst3/main.js
@@ -0,0 +1,13 @@
+var coffeeFlavor = "espresso";
+var coffeeTemprature = "piping hot";
+var coffeeOunces = 100;
+var coffeeMilk = true;
+
+var myCoffee = {
+flavor: "espresso",
+temprature: "piping hot",
+ounces: 100,
+milk: true
+};
+
+alert(myCoffee.ounces);
