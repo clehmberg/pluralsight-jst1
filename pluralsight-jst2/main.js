b3275532deba62f12303a1595a5a884ce5468572
clehmberg <clintlehmberg@gmail.com>
2017-09-11 21:19:17 -0600

    create pluralsight js lesson 2 assignment

--- pluralsight-jst2/main.js
+++ pluralsight-jst2/main.js
@@ -1,6 +1,38 @@
-function randomNumber(){
-  console.log(Math.round(Math.random() * 100));
+alert("You standing in your freshly tilled garden and you have a bag of magic seeds");
+var plant =prompt("do you want me to choose which seeds to plant? Yes or no.");
+if(plant === "yes"){
+  alert("eenie, meenie, miney, mo!");
+  var seeds = ["pumpkin", "squash", "tomato", "rhubarb",];
+  var pickseeds = seeds[randomNumber(seeds.length -1)];
+  function randomNumber(range){
+    return Math.round(Math.random() * range);
+  }
 }
-randomNumber();
-randomNumber();
-randomNumber();
+else{
+  var money = prompt("Do you want me to plant a magic money tree?");
+    if(money === "yes"){
+      alert("Money doesn't grow on tree's you idiot!");
+    }else if(money === "no"){
+      alert("You don't like money?...I guess I'll go take a nap then.");
+      throw error("the end");
+  }
+};
+
+console.log(pickseeds);
+
+switch(pickseeds){
+case "pumpkin":
+alert("You start planting magic pumpkin seeds");
+break;
+case "squash":
+alert("You start planting magic squash seeds");
+break;
+case "tomato":
+alert("You start planting magic tomato seeds");
+break;
+case "rhubarb":
+alert("You start planting magic rhubarb seeds");
+break;
+};
+var grow = (pickseeds);
+alert("The magic " + grow + " seeds start to grow and instantly bear ripe fruit");
